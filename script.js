00b15765742bdf90f219464429ff359ea40989e2
Dis-tech <santossobeyda8@gmail.com>
2026-02-27 13:09:47 -0400

    Created script.js for password generator functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+// Password Generator Functionality
+
+function generatePassword(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
+
+// Example usage:
+console.log(generatePassword(12)); // Generates a 12 character password
